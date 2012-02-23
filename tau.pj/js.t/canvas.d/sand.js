52fabbceadbedacf30115bc8b92e714d95f8d2b7
William Taysom <wtaysom@gmail.com>
2012-02-23 12:54:59 +0800

    Instead of dribbling once per step, try to empty out an overflowQueue and then dribble until a number of grainsPerInterval have been displayed.

--- tau.pj/js.t/canvas.d/sand.js
+++ tau.pj/js.t/canvas.d/sand.js
@@ -2,76 +2,100 @@
 
 /** Implementation of Per Bak's Sand Piles **/
 
+var grainSize = 5;
+var grainsPerInterval = 10;
+var interval = 1;
+var color = [
+		'#320', '#640', '#960',
+		'#B80', '#F00', '#F00',
+		'#F00', '#F00', '#F00'];
+
 var canvas;
 var ctx;
-var color = [	'rgb(255, 0, 0)', 'rgb(0, 255, 0)', 'rgb(0, 0, 255)',
-		'rgb(255, 255, 0)', 'rgb(0, 255, 255)', 'rgb(255, 0, 255)',
-		'rgb(255, 255, 255)', 'rgb(0, 0, 0)'];
-var stop = false;
-var size = 10;
 
-function sandpile(x, y) {
-	var a = new Array(x);
-	for (var i = 0; i < x; i++) {
-		a[i] = new Array(y);
-		for (var j = 0; j < y; j++) {
-			a[i][j] = 0;
-		}
-	}
-	return a;
-}
+var paused = false;
+var intervalId;
 
-function inc(a, x, y) {
-	if (x < 0 || x >= a.length) return;
-	if (y < 0 || y >= a[x].length) return;
-	if (++a[x][y] >= 4) {
-		a[x][y] -= 4;
-		inc(a, x + 1, y);
-		inc(a, x - 1, y);
-		inc(a, x, y + 1);
-		inc(a, x, y - 1);
-	}
-	ctx.fillStyle = color[a[x][y]];
-	ctx.fillRect(x * size, y * size, size, size);
-}
+var sandPile;
+var overflowQueue = [];
 
-function dribble(a) {
-	var x = Math.floor(Math.random() * a.length);
-	var y = Math.floor(Math.random() * a[x].length);
-	inc(a, x, y);
-}
-
-function drawSand(a) {
-	for (var x = 0; x < a.length; x++) {
-		for (var y = 0; y < a[x].length; y++) {
-			ctx.fillStyle = color[a[x][y]]; // 'rgb('+r+', 0, '+b+')';
-			ctx.fillRect(x * size, y * size, size, size);
+function initSandPile(width, height) {
+	sandPile = new Array(width);
+	for (var i = 0; i < width; i++) {
+		sandPile[i] = new Array(height);
+		for (var j = 0; j < height; j++) {
+			sandPile[i][j] = 0;
 		}
 	}
 }
 
-function runTests() {
-	canvas = $('#myCanvas').get(0);
+function drawGrain(x, y) {
+	ctx.fillStyle = color[sandPile[x][y]];
+	ctx.fillRect(x * grainSize, y * grainSize, grainSize, grainSize);
+}
+
+function overflow() {
+	overflowQueue.push(arguments);
+}
+
+function inc(x, y) {	
+	if (x < 0 || x >= sandPile.length) return;
+	if (y < 0 || y >= sandPile[x].length) return;
+	if (++sandPile[x][y] >= 4) {
+		sandPile[x][y] -= 4;
+		overflow(x + 1, y);
+		overflow(x - 1, y);
+		overflow(x, y + 1);
+		overflow(x, y - 1);
+	}
+	drawGrain(x, y);
+}
+
+function dribble() {
+	var x = Math.floor(Math.random() * sandPile.length);
+	var y = Math.floor(Math.random() * sandPile[x].length);
+	inc(x, y);
+}
+
+function pause() {
+	paused = true;
+	clearInterval(intervalId);
+}
+
+function resume() {
+	paused = false
+	clearInterval(intervalId);
+	intervalId = setInterval(function() {
+		for (var i = 0; i < grainsPerInterval; ++i) {
+			var overflow = overflowQueue.shift();
+			if (overflow) {
+				inc.apply(this, overflow)
+			} else {
+				dribble();
+			}
+		}
+	}, interval)
+}
+
+function run() {
+	canvas = $('#canvas').get(0);
 	ctx = canvas.getContext('2d');
-	$(canvas).click(function(e) {
-		stop = true;
+	
+	$('body').click(function(e) {
+		if (paused) {
+			resume();
+		} else {
+			pause();
+		}
 	});
 	
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	var xmax = Math.floor(canvas.height / size);
-	var ymax = Math.floor(canvas.width / size);
-	var a = sandpile(xmax, ymax);
-	$('body').css('backgroundColor', 'rgb(255, 255, 255)');
-	step();
-	function step() {
-		if (stop) return;
-		dribble(a);
-//		drawSand(a);
-		setTimeout(step, 1);
-	};
-		
+	var pileWidth = Math.floor(canvas.width / grainSize);
+	var pileHeight = Math.floor(canvas.height / grainSize);
+	initSandPile(pileWidth, pileHeight);
+	
+	resume();
 }
 
-$(runTests);
+$(run);
 
 })();
